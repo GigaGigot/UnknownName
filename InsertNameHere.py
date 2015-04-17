b3527093640216e2a357b010587f3cb034adf2bf
Théo Lassaube <theo-alexis@club-internet.fr>
2015-04-17 19:26:07 +0200

    Vie, attackspeed et plein d'autres  choses !

--- InsertNameHere.py
+++ InsertNameHere.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import sys, pygame
-import os
-import time
+import sys, os, time, thread
+import random, math
+import pygame
 from pygame.locals import *
-import random
-import thread
-import math
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+os.environ['SDL_VIDEO_WINDOW_POS'] = "40,35"
 
 
 def load_png(name):
@@ -41,6 +39,7 @@ def calculDirection(xA, yA, xB, yB):
 	direction = [vx/longueur, vy/longueur]
 	return direction
 
+
 def rotater(image, angle):
 	orig_rect = image.get_rect()
 	rot_image = pygame.transform.rotate(image, angle)
@@ -49,10 +48,38 @@ def rotater(image, angle):
 	rot_image = rot_image.subsurface(rot_rect).copy()
 	return rot_image
 
+
 def checkColisionTirSol(sol_rect, tir_sprite):
 	for sprite in tir_sprite.sprites():
 		if sol_rect.colliderect(sprite.rect):
 			sprite.kill()
+
+
+def essaiSpawn(chance):
+	rand = random.randint(1, chance)
+	if rand == chance:
+		cote = random.randint(1, 2)
+		if cote == 1:
+			return True, SCREEN_WIDTH + 180
+		else:
+			return True, -180
+	else:
+		return False, 0
+
+def dommagesEffectues(touche):
+	for tir in touche.keys():
+		for mob in touche[tir]:
+			mob.degatsPris(tir.element.degats)
+
+def dessinerBarreDeVie(screen ,ennemi):
+	x, y = ennemi.rect.bottomleft
+	w = ennemi.rect.width
+	vieB = ennemi.vieBase
+	vieC = ennemi.vieRestante
+	barre = pygame.draw.rect(screen, (0,0,0),(x, y, w, 5), 2)
+	remplisage = pygame.draw.rect(screen, (124,124,124),(x+1, y+1, w-1, 4))
+	vie = pygame.draw.rect(screen, (204,22,22),(x+1, y+1, (w-1) * ((vieB-(vieB-vieC))/vieB), 4))
+
 
 # Jade (joueur)
 class Jade(pygame.sprite.Sprite):    
@@ -67,12 +94,56 @@ class Jade(pygame.sprite.Sprite):
 
 
 class Element():
-	def __init__(self, nom, vitesse, poids, rotation):
+	def __init__(self, nom, vitesse, poids, rotation, degats, intervalle):
 		self.nom = nom
 		self.vitesse = vitesse
 		self.poids = poids
 		self.rotation = rotation
+		self.degats = degats
+		self.intervalle = intervalle
+		self.cpt_intervalle = 0
 
+class Ennemi(pygame.sprite.Sprite):    
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image, self.rect = load_png('Sprite/sprite_demon1.png')
+		self.cote = self.toFlipOrNotToFLip(x)
+		self.rect.center = [x, y]
+		# Attributs
+		self.vieBase = 10
+		self.vieRestante = 10
+		self.vitesseBase = 1
+		self.vitesseCourante = 1
+
+	def toFlipOrNotToFLip(self, x):
+		if x > SCREEN_WIDTH/2:
+			self.image = pygame.transform.flip(self.image, True, False)
+			return -1
+		return 1
+
+	def update(self, jade_rect, sol_rect):
+		self.checkHorsLimite()
+		if self.rect.right >= jade_rect.left and self.cote == 1:
+			pass
+		elif self.rect.left <= jade_rect.right and self.cote == -1:
+			pass
+		else:
+			self.rect = self.rect.move(self.vitesseCourante * self.cote, 0)
+
+		if self.rect.bottom >= sol_rect.top:
+			pass
+		else:
+			self.rect = self.rect.move(0, self.vitesseCourante * 10)
+
+	def checkHorsLimite(self):
+		if(self.rect.top < -100 or self.rect.bottom > 720 or self.rect.left < -300 or self.rect.right > 1580):
+			self.kill()
+
+	def degatsPris(self, degats):
+		self.vieRestante -= degats
+		if self.vieRestante <= 0:
+			self.vieRestante = 0
+			self.kill()
 
 class Tir(pygame.sprite.Sprite):
 	def __init__(self, element, x, y, direction):
@@ -86,7 +157,7 @@ class Tir(pygame.sprite.Sprite):
 		self.checkHorsLimite()
 		direction = [v * self.element.vitesse for v in self.direction]
 		self.rect = self.rect.move(direction)
-		self.image = rotater(self.image, self.element.rotation * self.element.vitesse)
+		self.image = rotater(self.image, self.element.rotation)
 		self.direction[1] += (self.element.poids/20)
 
 	def checkHorsLimite(self):
@@ -107,6 +178,7 @@ def main_function():
 	jade = Jade()
 	jade_sprite = pygame.sprite.RenderClear(jade)
 	tir_sprite = pygame.sprite.RenderClear()
+	ennemi_sprite = pygame.sprite.RenderClear()
 
 	# Images
 	background_image, background_rect = load_png('Background/bg1.png')
@@ -116,10 +188,10 @@ def main_function():
 	sol_rect.move_ip(0, 540)
 
 	# Zone de test
-	jade.ajouterElement(Element('feu', 20.0, 1.0, 1.0))
-	jade.ajouterElement(Element('glace', 10.0, 0.5, 1.0))
-	jade.ajouterElement(Element('foudre', 15.0, 0.0, 2.0))
-	jade.ajouterElement(Element('roche', 5.0, 1.0, 1.0))
+	jade.ajouterElement(Element('feu', 6.0, 0.2, 15.0, 4.0, 20.0))
+	jade.ajouterElement(Element('foudre', 8.0, 0.0, 13.0, 2.0, 30.0))
+	jade.ajouterElement(Element('glace', 4.0, 1.0, 11.0, 5.0, 90.0))
+	jade.ajouterElement(Element('roche', 3.0, 0.5, 9.0, 6.0, 60.0))
 
 	while True:
 		clock.tick(60)
@@ -130,15 +202,28 @@ def main_function():
 			if event.type == pygame.MOUSEBUTTONUP:
 				x, y = event.pos
 				for element in jade.liste_elements:
-					direction = calculDirection(jade.rect.centerx, jade.rect.centery, x, y)
-					tir = Tir(element, jade.rect.centerx, jade.rect.centery, direction)
-					tir_sprite.add(tir)
+					if element.cpt_intervalle >= element.intervalle:
+						element.cpt_intervalle = 0.0
+						direction = calculDirection(jade.rect.centerx, jade.rect.centery, x, y)
+						tir = Tir(element, jade.rect.centerx, jade.rect.centery, direction)
+						tir_sprite.add(tir)
 
 		# Check colisions
 		checkColisionTirSol(sol_rect, tir_sprite)
+		tirs_ennemis_touche = pygame.sprite.groupcollide(tir_sprite, ennemi_sprite, True, False, pygame.sprite.collide_circle_ratio(0.5))
+		dommagesEffectues(tirs_ennemis_touche)
+
+		# Spawn ennemis
+		spawn, xPos = essaiSpawn(100)
+		if spawn == True:
+			ennemi = Ennemi(xPos, SCREEN_HEIGHT/2)
+			ennemi_sprite.add(ennemi)
 
 		# Updatage
 		tir_sprite.update()
+		ennemi_sprite.update(jade.rect, sol_rect)
+		for element in jade.liste_elements:
+					element.cpt_intervalle += 1.0
 
 		# Blitage
 		screen.blit(background_image, background_rect)
@@ -147,6 +232,9 @@ def main_function():
 		# Drawage
 		jade_sprite.draw(screen)
 		tir_sprite.draw(screen)
+		ennemi_sprite.draw(screen)
+		for ennemi in ennemi_sprite:
+			dessinerBarreDeVie(screen ,ennemi)
 
 		# Infos
 		#newFont, posFont = createFont(None,40,"Nombre d'elements : "+str(len(tir_sprite)),(0,0,0),(0,0))
